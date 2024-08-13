94e908f94227c6ea2b39409cdafa9d5ca5667e14
kholoud93 <khloudayman97@gmail.com>
2024-08-14 01:25:24 +0300

    myportfolio

---
+++ src/Components/2-hero/Hero.jsx
@@ -0,0 +1,35 @@
+
+import "./hero.css";
+import Lottie from "lottie-react";
+import developerAnimation from "../../../public/animation/developer.json";
+import { motion } from "framer-motion";
+export default function Hero() {
+  return (
+    <section className="hero flex">
+<div className="left-section">
+  <div className="parent-avatar">
+    <motion.img
+     initial={{scale: 0}}
+     animate = {{scale : 1}}
+     transition ={{duration : 2}}
+     src="../../MyPic.png" className="avatar" alt="" />
+    <span className="icon-verified"></span>
+  </div>
+  <h1 
+ 
+  className="title">Frontend Developer</h1>
+  <p className="subtitle">Hi iam kholoud , a Frontend Developer React.js ambitious, detail oriented and passionate about creating engaging user experience.eager to contribute fresh ideas and skills to dynamic projects</p>
+<div className="icons flex">
+<div className="icon icon-twitter"></div>
+<div className="icon icon-instagram"></div>
+<div className="icon icon-github"></div>
+<div className="icon icon-linkedin-square"></div>
+</div>
+</div>
+<div className="right-section animation">
+<Lottie  animationData={developerAnimation} />
+</div>
+
+    </section>
+  )
+}
---
+++ src/Components/3-main/Main.jsx
@@ -0,0 +1,76 @@
+
+import { useState } from "react";
+import "./main.css";
+import { motion } from "framer-motion";
+export default function Main() {
+  const handleClick = (buttonCateogry)=>{
+    setCurrentActive(buttonCateogry);
+    const newArr = myProjects.filter((item)=>{
+      return item.category === buttonCateogry;
+    });
+    setArr(newArr)
+  
+  }
+  const myProjects=[
+    {projectTitle:"E-commerce",category:"react", imgPath:"/public/zz.jpg"},
+    {projectTitle:"Portfolio",category:"css", imgPath:"/public/6.jpg"},
+    {projectTitle:"Todo App",category:"javascript", imgPath:"/public/todo.png"},
+    {projectTitle:"Quotes",category:"javascript", imgPath:"/public/quotes.jpeg"},
+    {projectTitle:"Weather App",category:"javascript", imgPath:"/public/weather app.jpeg"},
+  ]
+ const[currentActive,setCurrentActive]= useState("all")
+ const[arr, setArr]= useState(myProjects)
+
+
+
+
+  return (
+ <main className="flex">
+  <section className="left-section flex">
+    <button className={currentActive === "all" ? "active": null} onClick={()=>{setCurrentActive("all"),
+    setArr(myProjects)
+    }}>All Projects</button>
+    <button className={currentActive === "css" ? "active": null} onClick={()=>{handleClick("css")  }}>Html && Css</button>
+    <button className={currentActive === "javascript" ? "active": null} onClick={()=>{handleClick("javascript") }}>Javascript</button>
+    <button className={currentActive === "react" ? "active": null} onClick={()=>{handleClick("react")}}>React</button>
+
+
+   
+  </section>
+  <section className="right-section flex">
+{arr.map((item)=>{
+  return(
+    
+  <motion.div
+layout 
+initial={{transform : "scale(0"}}
+animate = {{transform: "scale(1)"}}
+transform ={{type: "spring"}}
+
+  
+  className="card" key={item.imgPath}>
+  <img src={item.imgPath} width={250} alt="" />
+  <div style={{width:"250px"}}className="box">
+    <h1 className="title">{item.projectTitle}</h1>
+    <p className="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos rem 
+      suscipit cumque explicabo expedita minima pariatur hic fugit quidem impedit!</p>
+    <div className="flex icons">
+    <div className="flex" style={{gap:"10px"}}>
+    <div className="icon-link"></div>
+    <div className="icon-github"></div>
+    </div>
+      <a href="" className="link flex">more <span style={{alignSelf:"end"}} className="icon-arrow-right"></span></a>
+    </div>
+  </div>
+</motion.div>
+  )
+})}
+
+  
+
+
+  
+  </section>
+ </main>
+  );
+}
